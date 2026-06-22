ba4955cbd16da3a0a3d880b80a4efcdb9f751a95
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-06-22 19:37:59 +0530

    Fetch weather data from API

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ const apikey="ab7aff28dac1f8672e1e92b8307a851f";
 weatherform.addEventListener("submit", async event =>{
     event.preventDefault(); 
     const city = cityinput.value
+       if(city.trim()){
+        
+            const weatherdata = await getweatherdata(city);
+             console.log(weatherdata);
+       }
+        
 });
+
+async function getweatherdata(city) {
+
+ const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+ const response = await fetch(apiurl);
+ if(!response.ok){
+    throw new Error("could not fetch weather data!");
+ }
+else{
+    const data= await response.json();
+    return data;
+}
+   }
+
