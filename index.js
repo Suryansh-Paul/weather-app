0b933999a7a08eb27fa920a8b8082530e1d1b05d
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-06-22 19:44:30 +0530

    Add weather condition icons

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function displayweatherinfo(data){
     
     const descDisplay= document.createElement("p");
    
-    
+    const weatheremojidisplay= document.createElement("p");
   
 
     cityDisplay.textContent=city;
@@ -60,16 +60,51 @@ function displayweatherinfo(data){
      descDisplay.textContent=description;
     descDisplay.classList.add("descDisplay");
 
+     weatheremojidisplay.textContent= weatheremoji(id);
+    weatheremojidisplay.classList.add("weatherEmoji");
 
 
       card.appendChild(cityDisplay);
       card.appendChild(tempDisplay);
       card.appendChild(humidityDisplay);
       card.appendChild(descDisplay);
-    
+      card.appendChild(weatheremojidisplay);
 
 
 
 
 }
+
+function weatheremoji(weatherid){
+
+
+    switch(true){
+
+        case (weatherid >= 200 && weatherid < 300):
+            return "⛈️"; 
+
+        case (weatherid >= 300 && weatherid < 500):
+            return "🌦️"; 
+
+        case (weatherid >= 500 && weatherid < 600):
+            return "🌧️"; // Rain
+
+        case (weatherid >= 600 && weatherid < 700):
+            return "❄️"; 
+
+        case (weatherid >= 700 && weatherid < 800):
+            return "🌫️"; 
+        case (weatherid === 800):
+            return "☀️"; 
+
+        case (weatherid >= 801 && weatherid < 810):
+            return "☁️"; 
+
+        default:
+            return "❓";
+    }
+}
+
+
+
 
