c1e71fcb0496ce30fd6784477a74682d813ce79b
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-06-22 19:41:53 +0530

    Display weather information

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ weatherform.addEventListener("submit", async event =>{
        if(city.trim()){
         
             const weatherdata = await getweatherdata(city);
-             console.log(weatherdata);
-       }
+            displayweatherinfo(weatherdata);
+            
         
-});
-
+       }}     
+);
 async function getweatherdata(city) {
 
  const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
@@ -27,4 +27,49 @@ else{
     return data;
 }
    }
+
+function displayweatherinfo(data){
+   const{name:city, 
+      main:{temp,humidity},
+    weather:[{description, id}]}=data;
+
+    card.textContent="";
+    card.style.display="flex";
+    
+
+    const cityDisplay= document.createElement("h1");
+  
+    const tempDisplay= document.createElement("p");
+    
+    const humidityDisplay= document.createElement("p");
+    
+    const descDisplay= document.createElement("p");
+   
+    
+  
+
+    cityDisplay.textContent=city;
+    cityDisplay.classList.add("cityDisplay");
+
+     tempDisplay.textContent=`Temperature:${(temp-273.15).toFixed(1)}°C`;
+    tempDisplay.classList.add("tempDisplay");
+
+     humidityDisplay.textContent=`Humidity: ${humidity}%`;
+    humidityDisplay.classList.add("humidityDisplay");
+
+     descDisplay.textContent=description;
+    descDisplay.classList.add("descDisplay");
+
+
+
+      card.appendChild(cityDisplay);
+      card.appendChild(tempDisplay);
+      card.appendChild(humidityDisplay);
+      card.appendChild(descDisplay);
+    
+
+
+
+
+}
 
