8985120e076b2f773f2f0554f804aaf71ce88973
Suryansh-Paul <suryanshpaul65@gmail.com>
2026-06-22 19:31:03 +0530

    Implement weather search form

---
+++ index.js
@@ -0,0 +1,10 @@
+const weatherform = document.querySelector(".weatherform");
+const cityinput = document.querySelector(".cityinput");
+const card= document.querySelector(".card");
+const apikey="ab7aff28dac1f8672e1e92b8307a851f";
+
+
+weatherform.addEventListener("submit", async event =>{
+    event.preventDefault(); 
+    const city = cityinput.value
+});
